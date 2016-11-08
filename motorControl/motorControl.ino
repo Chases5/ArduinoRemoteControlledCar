5214b8a68114eaf3de6ef3d43466e665d6ffb6da
Chase Skelton <redghost50@gmail.com>
2016-11-08 15:48:52 -0800

    adding accessory controller

--- motorControl/motorControl.ino
+++ motorControl/motorControl.ino
@@ -6,28 +6,11 @@ AF_DCMotor motor2(2);
 AF_DCMotor motor3(3);
 AF_DCMotor motor4(4);
 
-void setup() {
-  // put your setup code here, to run once:
-  motor1.setSpeed(250);
-  motor2.setSpeed(250);
-  motor3.setSpeed(250);
-  motor4.setSpeed(250);
+void setup() 
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
   Serial.begin(9600);
 }
-
-//void receiveEvent(){
-  /*
-}
-  while(1 <Wire.available()){
-    char c = Wire.read();
-    Serial.print(c);zΩxΩΩ
-  }
-  Serial.println();
-  
-}
-*/
 void receiveEvent(int howMany) {
   while (1 < Wire.available()) { // loop through all but the last
     char c = Wire.read(); // receive byte as a character
@@ -37,12 +20,53 @@ void receiveEvent(int howMany) {
   Serial.println(x);         // print the integer
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void driveForward(){
+  motor1.setSpeed(250);
+  motor2.setSpeed(250);
+  motor3.setSpeed(250);
+  motor4.setSpeed(250);
   motor1.run(FORWARD);
-  //motor1.setSpeed(100);
   motor2.run(FORWARD);
   motor3.run(FORWARD);
   motor4.run(FORWARD);
-  //delay(500);
+}
+
+void driveReverse(){
+  motor1.setSpeed(250);
+  motor2.setSpeed(250);
+  motor3.setSpeed(250);
+  motor4.setSpeed(250);
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);
+}
+
+void driveRight(){
+  motor1.setSpeed(250);
+  motor1.run(FORWARD);
+  motor2.setSpeed(250);
+  motor2.run(FORWARD);
+  motor3.setSpeed(0);
+  motor4.setSpeed(0);
+}
+
+void driveLeft(){
+  motor1.setSpeed(0);
+  motor2.setSpeed(0);
+  motor3.setSpeed(250);
+  motor3.run(FORWARD);
+  motor4.setSpeed(250);
+  motor4.run(FORWARD);
+}
+
+void loop() {
+  driveForward();
+  delay(5000);
+  driveRight();
+  delay(5000);
+  driveLeft();
+  delay(5000);
+  driveReverse();
+  delay(5000);
 }
