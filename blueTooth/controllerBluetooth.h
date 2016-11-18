139371d028c3e2c837e17f0985cdbe543bc24357
TyGuy123321 <tylerc24@uw.edu>
2016-11-18 12:54:48 -0800

    Fixed Bluetooth

--- blueTooth/controllerBluetooth.h
+++ blueTooth/controllerBluetooth.h
@@ -7,15 +7,26 @@ void initializeBluetooth() {
 	Serial.begin(9600);
 }
 
-void sendButtonData(int* buttons) {
+void sendData(int* buttons) {
 	unsigned char data = packageButtonData(buttons);
 	Serial.print(data);
 }
 
-bool readData(int* values) {
+bool readData(String* direction, float* x, float* y) {
 	if (Serial.available()) {
-		unsigned char packet = (unsigned char) Serial.read();
-		readPackageButtons(packet, values);
+		unsigned char dir = (unsigned char) Serial.read();
+		unsigned char xBytes[4], yBytes[4];
+		for (int i = 0; i < 4; i++) {
+			xBytes[i]= (unsigned char) Serial.read();
+		}
+		for (int i = 0; i < 4; i++) {
+			yBytes[i] = (unsigned char) Serial.read();
+		}
+		
+		*direction = byteToDirection(dir);
+		*x = bytesToFloat(xBytes);
+		*y = bytesToFloat(yBytes);
+		
 		return true;
 	}
 	
