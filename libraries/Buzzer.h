7cc779bfe0964e5e6231f0780d05175bc76c8111
Chase Skelton <redghost50@gmail.com>
2016-12-08 14:15:28 -0800

    buzzer commenting

--- libraries/Buzzer.h
+++ libraries/Buzzer.h
@@ -1,3 +1,15 @@
+//==========================================================
+// Written by: Tyler Carlile and Chase Skelton
+// Date: 11/8/2016
+// Last Modification: 11/8/2016
+//==========================================================
+// Provided functions to set the buzzer to a pin and output
+// sound based on the horn and reverse conditions. on
+// reverse a pulsing buzzing noise will sound. When the horn
+// is set to on it will play a constant sound until the 
+// button is released.
+//==========================================================
+
 #ifndef BUZZER_H
 #define BUZZER_H
 #include <Arduino.h>
@@ -10,21 +22,42 @@ boolean buzzerCycle = true;
 const int BUZZER_TIME = 3000;
 boolean buzzerOn = true;
 
+/*
+ * Set the pin with the value of the passed in value as
+ * the output pin for the buzzer. No error checking is 
+ * done.
+ */
+
 void initBuzzer(int inputPin){
 	buzzerPin = inputPin;
 	pinMode(buzzerPin,OUTPUT);
 }
 
+/*
+ * Set the horn value to the passed in boolean. This will
+ * be used in the update buzzer function.
+ */
+
 void setHorn(boolean input){
 	buzzerHorn = input;
 }
+
+/*
+ * Set the reverse value to the passed in boolean. This will
+ * be used in the update buzzer function.
+ */
 
 void setReverse(boolean input){
 	buzzerReverse = input;
 }
 
+/*
+ * Update buzzer should be called in the loop of the accessory
+ * controller. It will check the value of setHorn and reverse
+ * and play the appropirate buzzing sound.
+ */
+
 void updateBuzzer() {
-  // put your main code here, to run repeatedly:
 	int msec = millis();
 	if(buzzerReverse && !buzzerHorn){
 		if(((msec - currentBuzzerTime) >= BUZZER_TIME)){
@@ -42,7 +75,5 @@ void updateBuzzer() {
 	if(buzzerHorn){
 		tone(buzzerPin,500);
 	}
-	
 }
-
 #endif
