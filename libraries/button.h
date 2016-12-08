633b0abfc86318d2eaf1435c865a6d0a8e1111fe
Chase Skelton <redghost50@gmail.com>
2016-12-08 14:18:39 -0800

    changed commenting for button

--- libraries/button.h
+++ libraries/button.h
@@ -1,3 +1,13 @@
+//==========================================================
+// Written by: Tyler Carlile and Chase Skelton
+// Date: 11/8/2016
+// Last Modification: 12/7/2016
+//==========================================================
+// The button functions are used the initialize and read 
+// The button pins. Pins must be chosen by the user and
+// will not be checked for errors.
+//==========================================================
+
 # ifndef BUTTON
 # define BUTTON
 int rightPin = 0;
@@ -7,6 +17,11 @@ int reversePin = 0;
 int hornPin = 0;
 
 enum{GO,REVERSE,LEFT,RIGHT,HORN};
+
+/*
+ * Initializes the pins of the passed in values. This does not
+ * do any error checking. 
+ */
 
 void initializeButtons(int right, int left, int forward, int reverse, int horn){
   pinMode(right, INPUT);
@@ -20,6 +35,12 @@ void initializeButtons(int right, int left, int forward, int reverse, int horn){
   pinMode(horn, INPUT);
   hornPin = horn;
 }
+
+/*
+ * Reads the buttons and sets them in place in the array
+ * at the position of the defined enum.
+ * ex. [GO,REVERSE, LEFT, RIGHT, HORN].
+ */
 
 void readButtons(int* retVal){
   retVal[GO] = digitalRead(forwardPin);
